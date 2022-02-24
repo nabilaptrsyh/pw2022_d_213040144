5a750fd6c01d335df51be974008b3032e1cac82c
nabilaptrsyh <lllalanabila300@gmail.com>
2022-02-24 11:05:30 +0700

    menyelesaikan tugas pertemuan 2

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,55 @@
+<?php
+// Pertemuan 2, belaja mengenal sintaks PHP
+// echo untuk mencetak nilai ke laya
+echo "<hr>";
+
+//OPERATOR
+// Aritmatika
+// +, -, *, /, %
+echo 5 % 2; // sisa bagi
+echo "<hr>";
+
+// Perbandingan 
+// <, >, <=, >=, ==, !=
+echo 10 != 20;
+echo "<hr>";
+
+// Variabel
+// Tempat menampung nilai
+// tidak boleh mengandung spasi
+// boleh mengandung angka, tapi tidak boleh diawal
+// snake_case : $nama_depan_mahasiswa
+// camelCase : namaDepanMahasiswa
+// kebab-case : nama-depan-mahasiswa
+$nama = 'nabila';
+$nama = 'putri';
+echo $nama;
+echo "<hr>";
+
+// Penugasan / Assigment
+// =, +=, -=, *=, /=, %=
+$a = 10;
+$a = 20;
+$a = 30;
+echo $a;
+echo "<hr>";
+
+// Increment & Decrement
+// ++, --
+$b = 10;
+$b--;
+echo $b;
+echo "<hr>";
+
+// Identitas 
+// ===, !==
+echo 10 === "10";
+?>
+
+' back tick
+~ tilde
+# pound
+^ caret 
+| pipe
+\ backslash
+/ slash 
