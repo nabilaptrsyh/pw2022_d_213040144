0edbebb1a210cd3800ef459daae9e1adb671539e
nabilaptrsyh <lllalanabila300@gmail.com>
2022-02-25 12:19:15 +0700

    menambahkan tugas pertemuan2

--- kuliah/pertemuan2/latihan1.php
+++ kuliah/pertemuan2/latihan1.php
@@ -53,3 +53,80 @@ echo 10 === "10";
 | pipe
 \ backslash
 / slash 
+
+<?php  
+// melanjutkan tugas pertemuan2
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// Var_dump
+
+echo "Nabila Putri Aisyah Insirawati";
+print "Nabila Putri Aisyah I";
+
+// Penulisan Sintaks PHP
+// 1. PHP di dalam html
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+$nama = "Nabila Putri";
+
+echo "Halo, nama saya $nama";
+
+// Operator
+// aritmatika
+// + - * / %
+echo 1 + 1;
+$x = 10;
+$y = 20;
+echo $x + $y;
+
+// Penggabung string / concattenation / concat
+// .
+$nama_depan = "Nabila";
+$nama_belakang = "Putri";
+echo $nama_depan . $nama_belakang;
+
+
+// Operator Assignment
+// =, +=, -=, *=, /=, %=, .=
+$x = 1;
+$x += 5;
+echo $x;
+
+$nama = "Nabila";
+$nama .= " ";
+$nama .= "Putri";
+echo $nama;
+
+
+// Operator Perbandingan
+// <, >, <=, >=, ==
+var_dump(1 < 5);
+
+// Operator Identitas
+// ===, !==
+var_dump(1 === "1")
+
+// Operator Logika
+// &&, ||, !
+$x = 10;
+var_dump($x < 20 && $x % 2 == 0);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Halo, Selamat Datang <?php echo $nama; ?></h1>
+    <p><?php "ini adalah paragraf"; ?></p>
+</body>
+</html>
